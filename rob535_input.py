df7bfcb91983bdddc5a9c64cdf598154dd230fd3
zhengant <zhengant@gmail.com>
2018-12-15 23:15:29 -0500

    init

---
+++ rob535_input.py
@@ -0,0 +1,78 @@
+import os
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import cv2
+import numpy as np
+import tensorflow as tf
+
+def generate_df(trainval_dir, test_dir, validation_split):
+    """
+    generates dfs for train, val, test data for creating PerceptionDataGenerator1
+    """
+    # get test data
+    test = []
+    for guid in os.listdir(test_dir):
+        if os.path.isdir(test_dir + guid):
+            for filename in os.listdir(test_dir + guid):
+                if filename[5] == 'c': # cloud image proj, pick cloud only
+                    test.append(guid + '/' + filename[:4]) # guid + / + 000x
+
+    test = pd.DataFrame(test, columns=['guid/image'])
+
+    ids_labels = pd.read_csv(trainval_dir + '/labels.csv')
+    train, val = train_test_split(ids_labels, test_size=validation_split)
+
+    return train, val, test
+                    
+
+class PerceptionDataGenerator1(tf.keras.utils.Sequence):
+    """
+    data generator object for task 1 of perception
+    """
+    def __init__(self, dir_name, data, image_generator=None, id_col='guid/image', label_col='label', batch_size=32,
+                 image_size=(224,224), n_classes=3):
+        self.dir_name = dir_name
+        self.data = data
+        self.id_col = id_col
+        self.label_col = label_col
+        self.image_generator = image_generator
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.n_classes = n_classes
+
+
+    def __len__(self):
+        return len(self.data) // self.batch_size
+
+
+    def __getitem__(self, index):
+        x = np.empty((self.batch_size, *self.image_size, 3))
+        y = np.empty(self.batch_size, dtype=int)
+
+        for batch_idx, data_idx in enumerate(range(index*self.batch_size, (index+1)*self.batch_size)):
+            x[batch_idx,] = self.__read_data__(self.data[self.id_col].values[data_idx])
+            if self.label_col is not None:
+                y[batch_idx] = self.data[self.label_col].values[data_idx]
+
+        return x if self.label_col is None else x, tf.keras.utils.to_categorical(y, self.n_classes)
+
+
+    def on_epoch_end(self):
+        self.data = self.data.sample(frac=1).reset_indexes(drop=True)
+
+
+    def __read_data__(self, id):
+        img = cv2.resize(cv2.imread(self.dir_name + '/' + id + '_image.jpg'), self.image_size).astype(np.float32)
+
+        # Subtract mean pixel and multiple by scaling constant using ImageNet mean/scales
+        # Reference: https://github.com/shicai/DenseNet-Caffe
+        img[:, :, 0] = (img[:, :, 0] - 103.94) * 0.017
+        img[:, :, 1] = (img[:, :, 1] - 116.78) * 0.017
+        img[:, :, 2] = (img[:, :, 2] - 123.68) * 0.017
+
+        if self.label_col is not None:
+            img = self.image_generator.random_transform(img)
+
+        return img
+
---
+++ rob535_task1.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+
+from densenet121_mod import DenseNet
+import rob535_input
+
+
+def create_model(n_classes):
+    model = DenseNet(classes=n_classes, weights_path='models/densenet121_weights_tf.h5')
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
+def train_model(model, train_gen, val_gen):
+    early_stop = tf.keras.callbacks.EarlyStopping(patience=5)
+    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath='best_model.h5', monitor='val_loss',
+                                                          save_best_only=True)
+    model.fit_generator(train_gen, epochs=100, callbacks=[early_stop, model_checkpoint], validation_data=val_gen)
+
+    return model
+
+
+def predict_test(model, test_input):
+    labels = model.predict_generator(test_input)
+
+    return labels
+
+
+def output_predicted_labels(test_df, labels, filename):
+    with open(filename, 'w') as out:
+        out.write('guid/image,label\n')
+        for i, label in enumerate(labels):
+            id = test_df['guid/image'].values[i]
+            out.write(id + ',' + str(label) + '\n')
+
+
+def main():
+    model = create_model(3)
+    train, val, test = rob535_input.generate_df('trainval', 'test', 0.2)
+    image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=15, width_shift_range=20,
+                                                                      height_shift_range=20, brightness_range=[0.5,1.5],
+                                                                      shear_range=15, zoom_range=0.5,
+                                                                      horizontal_flip=True)
+
+    train_gen = rob535_input.PerceptionDataGenerator1('trainval', train, image_generator)
+    val_gen = rob535_input.PerceptionDataGenerator1('trainval', val, image_generator)
+    test_gen = rob535_input.PerceptionDataGenerator1('testval', test, label_col=None)
+
+    train_model(model, train_gen, val_gen)
+    labels = predict_test(model, test_gen)
+
+    output_predicted_labels(test, labels, 'task1_out.csv')
+
+
+if __name__ == '__main__':
+    main()
